edc21d36f10011626838acac0c41881440bd9dbc
D3veate <devnull@localhost>
2012-12-02 18:48:04 -0800

    Added a basic dice roller.

--- src/com/bigtheta/ragedice/MainActivity.java
+++ src/com/bigtheta/ragedice/MainActivity.java
@@ -1,8 +1,12 @@
 package com.bigtheta.ragedice;
 
-import android.os.Bundle;
+import java.util.Random;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -19,4 +23,12 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    public void rollDice(View view) {
+    	Random rand = new Random();
+    	TextView tv1 = (TextView)findViewById(R.id.dice_result_1);
+    	tv1.setText(Integer.toString(rand.nextInt(6) + 1));
+    	
+    	TextView tv2 = (TextView)findViewById(R.id.dice_result_2);
+    	tv2.setText(Integer.toString(rand.nextInt(6) + 1));
+    }
 }
