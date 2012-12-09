f57dac771f3a692a8cb3a5a10261e6f24a49a1f9
Logan Evans <loganpevans@gmail.com>
2012-12-09 13:21:02 -0800

    Fixed Eclipse formatting artifacts.

--- src/com/bigtheta/ragedice/MainActivity.java
+++ src/com/bigtheta/ragedice/MainActivity.java
@@ -11,12 +11,12 @@ import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-	private int[] mDiceImgs;
-	private int mNumPlayers;
-	private int mPlayerNum;
-	private DiceRollDAO mDiceRollDAO;
-	
-	@Override
+    private int[] mDiceImgs;
+    private int mNumPlayers;
+    private int mPlayerNum;
+    private DiceRollDAO mDiceRollDAO;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -29,28 +29,28 @@ public class MainActivity extends Activity {
         mDiceImgs[5] = R.drawable.alea_6_transface_colbg;
         mNumPlayers = 2;
         mPlayerNum = 0;
-    	ImageView red_die = (ImageView)findViewById(R.id.red_die);
-    	red_die.setBackgroundColor(0xFFFF0000);
-    	ImageView yellow_die = (ImageView)findViewById(R.id.yellow_die);
-    	yellow_die.setBackgroundColor(0xFFFFFF00);
+        ImageView red_die = (ImageView)findViewById(R.id.red_die);
+        red_die.setBackgroundColor(0xFFFF0000);
+        ImageView yellow_die = (ImageView)findViewById(R.id.yellow_die);
+        yellow_die.setBackgroundColor(0xFFFFFF00);
 
-    	mDiceRollDAO = new DiceRollDAO(this);
-    	
-    	View mainView = (View)findViewById(R.id.activity_main_view);
-    	mainView.setBackgroundColor(0xFF818181);
-	}
-	
-	@Override
-	protected void onResume() {
-		super.onResume();
-		mDiceRollDAO.open();
-	}
-	
-	@Override
-	protected void onPause() {
-		super.onPause();
-		mDiceRollDAO.close();
-	}
+        mDiceRollDAO = new DiceRollDAO(this);
+
+        View mainView = (View)findViewById(R.id.activity_main_view);
+        mainView.setBackgroundColor(0xFF818181);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mDiceRollDAO.open();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mDiceRollDAO.close();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -58,62 +58,62 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-    
+
     protected int rollSingleDie(ImageView die) {
-    	Random rand = new Random();
-    	int die_select = rand.nextInt(6);
-    	die.setImageResource(mDiceImgs[die_select]);
-    	return die_select + 1;
+        Random rand = new Random();
+        int die_select = rand.nextInt(6);
+        die.setImageResource(mDiceImgs[die_select]);
+        return die_select + 1;
     }
-    
+
     protected void nextPlayer(Boolean goForward) {
-    	if (mNumPlayers > 1 && goForward) {
-    		mPlayerNum %= mNumPlayers;
-    		mPlayerNum++;
-    	} else {
-    		mPlayerNum--;
-    		if (mPlayerNum == 0) {
-    			mPlayerNum += mNumPlayers;
-    		}
-    	}
-		TextView player = (TextView)findViewById(R.id.player_number);
-		player.setText("Player " + Integer.toString(mPlayerNum));
+        if (mNumPlayers > 1 && goForward) {
+            mPlayerNum %= mNumPlayers;
+            mPlayerNum++;
+        } else {
+            mPlayerNum--;
+            if (mPlayerNum == 0) {
+                mPlayerNum += mNumPlayers;
+            }
+        }
+        TextView player = (TextView)findViewById(R.id.player_number);
+        player.setText("Player " + Integer.toString(mPlayerNum));
     }
-    
+
     protected void showTotals() {
-    	String resultsStr = "";
-    	for (int i = 2; i <= 12; i++) {
-    		resultsStr += "Num " + Integer.toString(i) +
-    					  " : " + Integer.toString(mDiceRollDAO.getCountForRoll(i)) +
-    					  " -- " + Double.toString(mDiceRollDAO.getExpectedCount(i)) + "\n";
-    	}
-    	resultsStr += "Prob1:" + Double.toString(mDiceRollDAO.calculateKSProbability());
-    	resultsStr += "\nProb2:" + Double.toString(mDiceRollDAO.calculateKSProbabilityMaximized());
-    	TextView results_view = (TextView)findViewById(R.id.dice_results);
-    	results_view.setText(resultsStr);
+        String resultsStr = "";
+        for (int i = 2; i <= 12; i++) {
+            resultsStr += "Num " + Integer.toString(i) +
+                " : " + Integer.toString(mDiceRollDAO.getCountForRoll(i)) +
+                " -- " + Double.toString(mDiceRollDAO.getExpectedCount(i)) + "\n";
+        }
+        resultsStr += "Prob1:" + Double.toString(mDiceRollDAO.calculateKSProbability());
+        resultsStr += "\nProb2:" + Double.toString(mDiceRollDAO.calculateKSProbabilityMaximized());
+        TextView results_view = (TextView)findViewById(R.id.dice_results);
+        results_view.setText(resultsStr);
     }
-    
+
     public void resetDiceRolls(View view) {
-    	mDiceRollDAO.deleteAllDiceRolls();
-    	mPlayerNum = 1;
-    	showTotals();
+        mDiceRollDAO.deleteAllDiceRolls();
+        mPlayerNum = 1;
+        showTotals();
     }
-    
+
     public void undoDiceRoll(View view) {
-    	// TODO: Check if the rolls are empty. This crashes otherwise.
-    	mDiceRollDAO.deleteDiceRoll(mDiceRollDAO.getLastDiceRoll());
-    	nextPlayer(false);
-		showTotals();
+        // TODO: Check if the rolls are empty. This crashes otherwise.
+        mDiceRollDAO.deleteDiceRoll(mDiceRollDAO.getLastDiceRoll());
+        nextPlayer(false);
+        showTotals();
     }
-    	
+
     public void rollDice(View view) {
-    	int die_roll_result = 0;
-    	nextPlayer(true);
-    	ImageView red_die = (ImageView)findViewById(R.id.red_die);
-    	ImageView yellow_die = (ImageView)findViewById(R.id.yellow_die);
-    	die_roll_result += rollSingleDie(red_die);
-    	die_roll_result += rollSingleDie(yellow_die);
-    	mDiceRollDAO.createDiceRoll(die_roll_result);
-    	showTotals();
+        int die_roll_result = 0;
+        nextPlayer(true);
+        ImageView red_die = (ImageView)findViewById(R.id.red_die);
+        ImageView yellow_die = (ImageView)findViewById(R.id.yellow_die);
+        die_roll_result += rollSingleDie(red_die);
+        die_roll_result += rollSingleDie(yellow_die);
+        mDiceRollDAO.createDiceRoll(die_roll_result);
+        showTotals();
     }
 }
--- src/com/bigtheta/ragedice/MySQLiteHelper.java
+++ src/com/bigtheta/ragedice/MySQLiteHelper.java
@@ -6,34 +6,34 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class MySQLiteHelper extends SQLiteOpenHelper {
-	public static final String TABLE_DICE_ROLLS = "dice_rolls";
-	public static final String COLUMN_ID = "_id";
-	public static final String COLUMN_ROLL_RESULT = "roll_results";
+    public static final String TABLE_DICE_ROLLS = "dice_rolls";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_ROLL_RESULT = "roll_results";
 
-	private static final String DATABASE_NAME = "dice_roll.db";
-	private static final int DATABASE_VERSION = 1;
-	
-	// Database creation sql statement
-	private static final String DATABASE_CREATE = "create table "
-			+ TABLE_DICE_ROLLS + "("
-			+ COLUMN_ID + " integer primary key autoincrement, "
-			+ COLUMN_ROLL_RESULT + " int not null);";
+    private static final String DATABASE_NAME = "dice_roll.db";
+    private static final int DATABASE_VERSION = 1;
 
-	public MySQLiteHelper(Context context) {
-		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-	}
+    // Database creation sql statement
+    private static final String DATABASE_CREATE = "create table "
+        + TABLE_DICE_ROLLS + "("
+        + COLUMN_ID + " integer primary key autoincrement, "
+        + COLUMN_ROLL_RESULT + " int not null);";
 
-	@Override
-	public void onCreate(SQLiteDatabase database) {
-		database.execSQL(DATABASE_CREATE);
-	}
+    public MySQLiteHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		Log.w(MySQLiteHelper.class.getName(),
-				"Upgrading database from version " + oldVersion + " to "
-						+ newVersion + ", which will destroy all old data");
-		db.execSQL("DROP TABLE IF EXISTS " + TABLE_DICE_ROLLS);
-		onCreate(db);
-	}
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(MySQLiteHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_DICE_ROLLS);
+        onCreate(db);
+    }
 }
