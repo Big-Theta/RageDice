0b9bd788097521da92610b83038100fa930a6402
cevans87 <devnull@localhost>
2013-02-09 17:20:38 -0800

    Fixed DiceDisplayFragment displayUpdate problem in onResume.

--- src/com/bigtheta/ragedice/DiceDisplayFragment.java
+++ src/com/bigtheta/ragedice/DiceDisplayFragment.java
@@ -1,10 +1,9 @@
 package com.bigtheta.ragedice;
 
-import java.lang.reflect.Field;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -57,6 +56,7 @@ public class DiceDisplayFragment extends Fragment  {
 
     @Override
     public void onResume() {
+        displayDiceRoll(DiceRoll.getLastDiceRoll(MainActivity.getGame().getId()));
         super.onResume();
     }
 
@@ -97,11 +97,13 @@ public class DiceDisplayFragment extends Fragment  {
         //TextView tv = (TextView) m_callback.findViewById(R.id.player_number);
         //Player currentPlayer = Player.retrieve(dr.getPlayerId());
         //tv.setText(currentPlayer.getPlayerName());
+        Log.e("DDF", "gets here 1");
         if (dr == null) {
             Player initPlayer = Player.getNextPlayer(MainActivity.getGame().getId());
             dr = new DiceRoll(initPlayer);
             displayDiceRoll(dr);
             dr.delete();
+            Log.e("DDF", "gets here 2");
         }else {
             Class<drawable> res = R.drawable.class;
             for (DieResult result : DieResult.getDieResults(dr)) {
@@ -114,6 +116,7 @@ public class DiceDisplayFragment extends Fragment  {
                 } catch (Exception err) {
                 }
             }
+            Log.e("DDF", "gets here 3");
         }
         refreshStatusText();
     }
--- src/com/bigtheta/ragedice/TabsFragment.java
+++ src/com/bigtheta/ragedice/TabsFragment.java
@@ -1,17 +1,16 @@
 package com.bigtheta.ragedice;
 
-import com.bigtheta.ragedice.GameLogFragment.GameLogListener;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTabHost;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class TabsFragment extends Fragment {
+public class TabsFragment extends Fragment implements FragmentTabHost.OnTabChangeListener {
     private FragmentTabHost m_tabHost;
     TabsFragmentListener m_callback;
 
@@ -47,18 +46,22 @@ public class TabsFragment extends Fragment {
                 "Player Time Histogram", getResources().getDrawable(R.drawable.histo_tab_selected)), HistogramPlayerTimeFragment.class, null);
         m_tabHost.addTab(m_tabHost.newTabSpec("ksdf").setIndicator(
                 "Statistics", getResources().getDrawable(R.drawable.stats_tab_selected)), KSDescriptionFragment.class, null);
+        m_tabHost.setOnTabChangedListener(this);
         return m_tabHost;
     }
+    
 
     public void refreshDisplay() {
+        Log.e("in TabsFragment", "refreshDisplay was called");
         long gameId = MainActivity.getGame().getId();
         FragmentManager fm = getChildFragmentManager();
-        Fragment c_fragment = fm.findFragmentById(R.id.tabs_content_container);
+        Fragment c_fragment = fm.findFragmentById(R.layout.tabs_content_layout);
         if (c_fragment == null) {
             throw new IllegalStateException("Tabs container contains no fragments.");
         } else {
             if (c_fragment instanceof DiceDisplayFragment) {
                 ((DiceDisplayFragment)c_fragment).displayDiceRoll(DiceRoll.getLastDiceRoll(gameId));
+                Log.e("in TabsFragment", "refreshDisplay called displayDiceRoll");
             } else if (c_fragment instanceof KSDescriptionFragment) {
                 ((KSDescriptionFragment)c_fragment).displayInfo(gameId);
             } else if (c_fragment instanceof GameLogFragment) {
@@ -67,8 +70,11 @@ public class TabsFragment extends Fragment {
                 ((GameLogFragment)c_fragment).displayInfo(nextPlayer, dr);
             } else if (c_fragment instanceof HistogramRollsFragment) {
                 ((HistogramRollsFragment)c_fragment).updateHistogram();
+                Log.e("in TabsFragment", "c_fragment is hgf");
             } else if (c_fragment instanceof HistogramPlayerTimeFragment) {
                 ((HistogramPlayerTimeFragment)c_fragment).updateHistogram();
+            }else {
+                Log.e("in TabsFragment", "did not refresh any tabs");
             }
         }
     }
@@ -87,5 +93,8 @@ public class TabsFragment extends Fragment {
 
     public int getTabNumber() {
         return m_tabHost.getCurrentTab();
+    }
+    
+    public void onTabChanged(String tabId) {
     }
 }
